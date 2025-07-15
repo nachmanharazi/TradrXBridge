55cdd7ace9fe1011d63961d080d5013f5257e736
nachmanharazi <nchmann1@gmail.com>
2025-07-15 16:28:09 +0300

    Remove .github workflows directory

--- .github/workflows/minimal-test.js
+++
@@ -1,8 +0,0 @@
-// Minimal test script to verify Docker can run Node.js
-const fs = require('fs');
-
-// This will create a file in the current directory
-fs.writeFileSync('minimal-test-output.txt', 'Hello from Docker! ' + new Date().toISOString());
-
-// This will be visible in the Docker logs
-console.log('Minimal test script completed successfully!');
